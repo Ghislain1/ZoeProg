49e807b78c8e9741e4fb3da1167b96d4b427466f
GHislain1 <Gzeleu@gmx.de>
2018-07-04 11:54:31 +0200

    My Control

---
+++ src_GhisC/Ghis_UI_For_WPF/Ghis.Controls/GhisTextBox.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Ghis.Controls
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using System.Windows.Controls;
+  public class GhisTextBox : TextBox
+  {
+
+
+  }
+}
