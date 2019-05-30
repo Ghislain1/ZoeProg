7d53f606ff8574cae9acd180f8839ddd62ae4216
Ghis <Gzeleu@protonmail.com>
2019-05-30 17:14:27 +0200

    Style has been added

---
+++ src/ZoeProg.CustomControlLib/ZoeProgShell.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZoeProg.CustomControlLib
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ZoeProg.CustomControlLib"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ZoeProg.CustomControlLib;assembly=ZoeProg.CustomControlLib"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class ZoeProgShell : Window
+    {
+        private Storyboard backgroundAnimation;
+        //static ZoeProgShell()
+        //{
+        //    DefaultStyleKeyProperty.OverrideMetadata(typeof(ZoeProgShell), new FrameworkPropertyMetadata(typeof(ZoeProgShell)));
+        //}
+        public ZoeProgShell()
+        {
+            this.DefaultStyleKey = typeof(ZoeProgShell);
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
+        }
+
+        /// <summary>
+        /// Identifies the BackgroundContent dependency property.
+        /// </summary>
+        public static readonly DependencyProperty BackgroundContentProperty = DependencyProperty.Register("BackgroundContent", typeof(object), typeof(ZoeProgShell));
+
+        /// <summary>
+        /// Identifies the IsTitleVisible dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsTitleVisibleProperty = DependencyProperty.Register("IsTitleVisible", typeof(bool), typeof(ZoeProgShell), new PropertyMetadata(false));
+
+        /// <summary>
+        /// Identifies the LogoData dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LogoDataProperty = DependencyProperty.Register("LogoData", typeof(Geometry), typeof(ZoeProgShell));
+
+
+        private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+        private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+        private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void OnRestoreWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.ResizeMode == ResizeMode.CanResize || this.ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.ResizeMode != ResizeMode.NoResize;
+        }
+
+        /// <summary>
+        /// Gets or sets the background content of this window instance.
+        /// </summary>
+        public object BackgroundContent
+        {
+            get { return GetValue(BackgroundContentProperty); }
+            set { SetValue(BackgroundContentProperty, value); }
+        }
+
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the window title is visible in the UI.
+        /// </summary>
+        public bool IsTitleVisible
+        {
+            get { return (bool)GetValue(IsTitleVisibleProperty); }
+            set { SetValue(IsTitleVisibleProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the path data for the logo displayed in the title area of the window.
+        /// </summary>
+        public Geometry LogoData
+        {
+            get { return (Geometry)GetValue(LogoDataProperty); }
+            set { SetValue(LogoDataProperty, value); }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, is invoked whenever application code or internal processes call System.Windows.FrameworkElement.ApplyTemplate().
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // retrieve BackgroundAnimation storyboard
+            var border = GetTemplateChild("WindowBorder") as Border;
+            if (border != null)
+            {
+                this.backgroundAnimation = border.Resources["BackgroundAnimation"] as Storyboard;
+
+                if (this.backgroundAnimation != null)
+                {
+                    this.backgroundAnimation.Begin();
+                }
+            }
+        }
+
+    }
+}
--- src/ZoeProg/Views/Shell.xaml.cs
+++ src/ZoeProg/Views/Shell.xaml.cs
@@ -1,16 +1,18 @@
-﻿using MahApps.Metro.Controls;
+﻿
 
 namespace ZoeProg.Views
 {
+ 
+    using ZoeProg.CustomControlLib;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Shell : MetroWindow
+    public partial class Shell : ZoeProgShell
     {
         public Shell()
         {
             InitializeComponent();
-            // LoadLinks();
+         
         }
     }
 }
