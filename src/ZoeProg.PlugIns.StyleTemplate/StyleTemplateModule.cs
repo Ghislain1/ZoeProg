535660fba7992c2cdc44e31d17d7c4bb534be7c4
Ghis <Gzeleu@protonmail.com>
2019-06-06 13:00:07 +0200

    Construction of Style and Template

---
+++ src/ZoeProg.PlugIns.StyleTemplate/StyleTemplateModule.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ZoeProg.PlugIns.StyleTemplate
+{
+    using Prism.Ioc;
+    using Prism.Modularity;
+    using Prism.Regions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ZoeProg.PlugIns.StyleTemplate.Views;
+
+    public class StyleTemplateModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("TabRegion", typeof(StyleTemplateView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ src/ZoeProg.PlugIns.StyleTemplate/ViewModels/StyleTemplateViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ZoeProg.PlugIns.StyleTemplate.ViewModels
+{
+    using Prism.Mvvm;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class StyleTemplateViewModel : BindableBase
+    {
+        public string Header { get; } = "Style&Template";
+    }
+}
