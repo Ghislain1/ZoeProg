04f2158820f4215d7695b5dcb55d060d2d8fc9a3
Ghis <Gzeleu@protonmail.com>
2019-07-28 11:53:58 +0200

    add style metro.mahapp

--- src/ZoeProg/Views/Shell.xaml.cs
+++ src/ZoeProg/Views/Shell.xaml.cs
@@ -1,12 +1,13 @@
 ﻿namespace ZoeProg.Views
 {
   using System.Windows;
+    using MahApps.Metro.Controls;
 
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class Shell : Window
-  {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : MetroWindow
+    {
     public Shell()
     {
       InitializeComponent();
