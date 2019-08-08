5f5317aa0909b25d494ac6f46b0e63802d2ad17d
GhislainTHE1 <gzeleu@web.de>
2019-08-08 17:22:30 +0200

    Add clean service

---
+++ src/ZoeProg.Cleanup/Services/ICleanupService.cs
@@ -0,0 +1,56 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZoeProg.Common;
+
+namespace ZoeProg.Cleanup.Services
+{
+  public interface ICleanupService
+  {
+    List<string> GetAll();
+
+    Task CleanTempFilesAsync();
+  }
+
+  public class TempFileEvent : PubSubEvent
+  {
+  }
+
+  public class CleanupService : ICleanupService
+  {
+    private readonly IPowerShellService powerShellService;
+
+    private readonly IEventAggregator eventAggregator;
+    private List<string> tempFiles;
+
+    public CleanupService(IPowerShellService powerShellService, IEventAggregator eventAggregator)
+    {
+      this.powerShellService = powerShellService ?? throw new ArgumentNullException(nameof(powerShellService));
+      this.eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
+
+      this.DoAsync().GetAwaiter();
+    }
+
+    public async Task CleanTempFilesAsync()
+    {
+      var cmd = "Get-ChildItem $env:TEMP | foreach { Remove-Item $_}";
+      var sd = await this.powerShellService.RunCommand<string>(cmd, "List");
+      await DoAsync();
+    }
+
+    public List<string> GetAll()
+    {
+      return this.tempFiles;
+    }
+
+    private async Task DoAsync()
+    {
+      await Task.Delay(1000);
+      var cmd = "Get-ChildItem $env:TEMP";
+      var sd = await this.powerShellService.RunCommand<string>(cmd, "List");
+      this.tempFiles = new List<string>(sd);
+      this.eventAggregator.GetEvent<TempFileEvent>().Publish();
+    }
+  }
+}
